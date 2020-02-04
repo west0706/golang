8c07875e7dbe76e9bfe7abc3c9e542f006870798
west0706 <west0706@gmail.com>
2020-02-04 10:12:26 +0900

    add mini game

--- test/src/mini_game_ex.go
+++ test/src/mini_game_ex.go
@@ -13,14 +13,42 @@ func main() {
 	winRate[1] = 0
 	winRate[2] = 0
 
-	// for i := 0; i < 100; i++ {
-	card := makeCard()
-	// fmt.Println(card)
+	for i := 0; i < 100; i++ {
+		card := makeCard()
+		mycard := shuffleCard(card)
+		fmt.Println(mycard)
 
-	mycard := shuffleCard(card)
-	fmt.Println(mycard)
+		var player_1, player_2 [2]int
+		player_1[0] = mycard[0]
+		player_2[0] = mycard[1]
+		player_1[1] = mycard[2]
+		player_2[1] = mycard[3]
 
-	// }
+		player_1_result, player_2_result, winner := compatition(player_1, player_2)
+		fmt.Println(player_1_result, player_2_result, winner)
+		winRate[winner]++
+	}
+
+	fmt.Println(winRate)
+
+	fmt.Printf("Player1 Win Rate : %0.2f %%\n", float32(winRate[1]))
+	fmt.Printf("Player2 Win Rate : %0.2f %%\n", float32(winRate[2]))
+	fmt.Printf("Draw Rate : %0.2f %%\n", float32(winRate[0]))
+}
+
+func compatition(player_1 [2]int, player_2 [2]int) (int, int, int) {
+
+	player_1_result := (player_1[0] + player_1[1]) % 10
+	player_2_result := (player_2[0] + player_2[1]) % 10
+	winner := 0
+	if player_1_result > player_2_result {
+		winner = 1
+	} else if player_1_result == player_2_result {
+		winner = 0
+	} else {
+		winner = 2
+	}
+	return player_1_result, player_2_result, winner
 }
 
 func makeCard() [20]int {
@@ -61,11 +89,8 @@ func shuffleCard(card [20]int) [20]int {
 		}
 
 		fmt.Println(randomNumber)
-
 	}
-
 	return myCard
-
 }
 
 func checkCard(card [20]int) int {
