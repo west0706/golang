62cb900ad80526ff8831f55454f01c13130e1fb0
west0706 <west0706@gmail.com>
2020-02-06 10:32:21 +0900

    add tcp server client

---
+++ test/src/mypackage/package1.go
@@ -0,0 +1,5 @@
+package Test
+
+import "fmt"
+
+func
---
+++ test/src/tcp_client1.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+	connect, err := net.Dial("tcp", "127.0.0.1:5555")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	for {
+		//hearbeat
+		connect.Write([]byte("HIHI"))
+		fmt.Println("Send Data:", "HIHI")
+		time.Sleep(time.Second * 1)
+	}
+
+}
