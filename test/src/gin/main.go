e6eef15d5347639d1fc17b04f33d4c540221bb4c
west0706 <west0706@gmail.com>
2020-02-06 16:36:32 +0900

    all source

--- test/src/gin/main.go
+++ test/src/gin/main.go
@@ -4,5 +4,10 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	r := gin.Default()
-	fmt.println(r)
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "TEST pong",
+		})
+	})
+	r.Run()
 }
